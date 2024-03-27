958be71051a4474ff4f8c20a8932e3e18ae9f06a
deskastudio <mulyanadeska85@gmail.com>
2024-03-27 13:53:30 +0700

    end commit

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about', methods=['GET'])
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/education', methods=['GET'])
+@app.route('/education')
 def education():
     return render_template('education.html')
 
-@app.route('/experience', methods=['GET'])
+@app.route('/experience')
 def experience():
     return render_template('experience.html')
 
-@app.route('/portofolio', methods=['GET'])
+@app.route('/portofolio')
 def portofolio():
     return render_template('portofolio.html')
 
